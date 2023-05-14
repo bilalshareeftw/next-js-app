ff8216fd044ba7fa98c87c72ba9de772607a93cb
Mohammed Bilal Shareef <bilal.shareef@thoughtworks.com>
2023-05-14 21:47:24 +0530

    Update config for deploying in Github Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: 'export',
+  basePath: '/next-js-app',
+  experimental: {
+    appDir: true,
+  },
+}
 
 module.exports = nextConfig
